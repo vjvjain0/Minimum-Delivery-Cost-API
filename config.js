b7951040cf178d1579a7964dad69bd364ac62491
Vaibhav Jain <51946784+vjvjain0@users.noreply.github.com>
2019-06-28 03:02:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 config={}
 
 config.graph=[[0,4,Infinity,3],
-				  [4,0,3,2.5],
-		          [Infinity,3,0,2],
-		          [3,2.5,2,0]];
+	      [4,0,3,2.5],
+	      [Infinity,3,0,2],
+	      [3,2.5,2,0]];
 
 config.fixedRate = 10;
 config.limit = 5;
